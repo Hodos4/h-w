fc1bee22a24a59f7f80be6bbb8e5d0a9ef6e417f
Hodos4 <56293477+Hodos4@users.noreply.github.com>
2019-12-24 10:57:27 +0300

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,22 @@
+<?php
+$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+
+$pass= md5($pass."hghghg123");
+
+$mysql = new mysqli('localhost', 'root', 'root', 'register-bd');
+
+$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass'");
+$user = $result->fetch_assoc();
+if(count($user) == 0) {
+  echo "Такой пользователь не найден";
+  exit();
+}
+
+setcookie('user', $user['name'], time() + 3600, "/" );
+
+
+$mysql->close();
+
+header('Location: /');
+?>
