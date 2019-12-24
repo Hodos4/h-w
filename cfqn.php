02fde2db000a9c11eca6f62faea03ffdecb8adfe
Hodos4 <56293477+Hodos4@users.noreply.github.com>
2019-12-24 10:54:10 +0300

    Create cfqn.php

---
+++ cfqn.php
@@ -0,0 +1,165 @@
+body {
+     background:url(http://inshatura.ru/upload/resizeproxy/720_/8f000dadb1f35fb2f5e902379990b153.jpg?1549456311);
+     background-size : 100%;
+     }
+#заголовок{
+     height:60px;
+           background: #C0C0C0;
+           outline: 2px solid #000;
+           border: 3px solid #fff;
+           border-radius: 10px;
+          }
+
+#b {
+  margin: auto;
+  width: 300px;
+  height: 100px;
+    background: #C0C0C0;
+    border: 3px solid #fff;
+    border-radius: 10px;
+   }
+
+#c {
+  float: left;
+  color : red;
+  width : 250px;
+   }
+#c:hover{
+       color : yellow;
+    }
+
+#c:visited {
+          color : #800080;
+       }
+#d {
+  float: left;
+  color : red;
+  width : 600px;
+  margin: auto;
+  padding-left:50px;
+  padding-top:50px;
+  }
+
+#d:hover{
+       color : yellow;
+    }
+
+#d:visited {
+          color : #800080;
+       }
+
+ #e {
+  float: right;
+  color : red;
+  width : 250px;
+  margin: auto;
+  padding-top:1px;
+  padding-right: 70px;
+    }
+
+#e:hover{
+       color : yellow;
+    }
+
+#e:visited {
+          color : #800080;
+       }
+
+#контактнаяинформация {
+                    position: fixed;
+                      left: 0; bottom: 0;
+                      padding: 10px;
+                      background:#C0C0C0 ;
+                      color: #fff;
+                      width: 100%;
+                      }
+
+#Tell:hover {
+          color : yellow;
+        }
+
+#E-mail:hover {
+          color : yellow;
+        }
+
+#классика {
+     height:60px;
+     width : 500px;
+           background: #C0C0C0;
+           outline: 2px solid #000;
+           border: 3px solid #fff;
+           border-radius: 10px;
+     float: left;
+          }
+
+#свободный {
+     height:60px;
+     width : 500px;
+           background: #C0C0C0;
+           outline: 2px solid #000;
+           border: 3px solid #fff;
+           border-radius: 10px;
+     float: left;
+          }
+
+ #кспид {
+  float: left;
+  color : black;
+  width : 600px;
+  margin: auto;
+  }
+
+#кспид:hover{
+       color : red;
+    }
+
+#кспид:visited {
+          color : #800080;
+       }
+
+#сспид {
+    float: left;
+  color : black;
+  width : 600px;
+  margin: auto;
+  padding-left:50px;
+  }
+
+#сспид:hover{
+       color : red;
+    }
+
+#сспид:visited {
+          color : #800080;
+       }
+
+#камус {
+    float: left;
+  color : black;
+  width : 600px;
+  margin: auto;
+  padding-right:70px;
+  }
+
+#камус:hover{
+       color : red;
+    }
+
+#камус:visited {
+          color : #800080;
+       }
+
+ #sal {
+    float: left;
+  color : blue;
+  width : 600px;
+  margin: auto;
+  padding-left:50px;cfqn.
+  }
+
+#sal:hover{
+       color : red;
+    }
+
+#sal:visited {
+          color : #800080;
